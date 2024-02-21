c971cd60ed11ee77df3f47d05db15687eee9fe73
Nihar0071 <niharpatel007rd@gmail.com>
2024-02-21 17:28:49 +0530

    Components Made and Images added

---
+++ components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import React, {useEffect,useState} from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+// Import Icon 
+
+import {MdNotifications} from "react-icons/md";
+import {BsSearch} from "react-icons/bs";
+import {CgMenuLeft, CgMenuRight} from "react-icons/cg";
+
+
+//Internal Import 
+
+import Style from "./NavBar.module.css";
+import {Discover, HelpCenter, Notification, Profile, Sidebar} from "./index";
+import Button from "../componentsindex";
+
+const NavBar = () => { 
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
---
+++ components/NavBar/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//Internal Import
+
+import Style from "./Sidebar.module.css"
+const Sidebar = () => {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
