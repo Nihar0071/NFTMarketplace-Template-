0b209d99167ac30e49d8c64663f6b8879bf2fb0b
Nihar0071 <niharpatel007rd@gmail.com>
2024-03-07 10:17:03 +0530

    Footer Done

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -32,6 +32,7 @@ const Footer = () => {
                   <TiSocialYoutube/>
                 </a>
             </div>
+        </div>
             <div className={Style.footer_box_discover}>
               <h3>Discover</h3>
               <Discover/>
@@ -53,7 +54,7 @@ const Footer = () => {
                 </p>
               </div>
             </div>
-        </div>
+        
       </div>
     </div>
   )
--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,9 @@ import { NavBar,Footer } from "@/components/componentsindex";
 const MyApp = ({ Component, pageProps }) =>(
     <div>
         <NavBar/>
-        <Footer/>
         <Component {...pageProps} />
+        <Footer/>
+
     </div>
 );
 
